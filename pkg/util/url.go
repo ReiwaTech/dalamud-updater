04c8d19efd11e7d5bef423e9731f66b793e0d5b3
Innpaul Zhang <zyp108421@gmail.com>
2024-08-27 20:33:08 +0800

    fix: update assets url

--- pkg/util/url.go
+++ pkg/util/url.go
@@ -5,7 +5,7 @@ import "fmt"
 const distrib = "https://reiwatech.github.io/dalamud-distrib"
 const dotnet = "https://dotnetcli.azureedge.net/dotnet"
 
-const DalamudAssets = "https://goatcorp.github.io/DalamudAssets/asset.json"
+const DalamudAssets = "https://reiwatech.github.io/DalamudAssets/asset.json"
 const NotoSansCJKscRegular = "https://mirrors.tuna.tsinghua.edu.cn/ctan/fonts/notocjksc/NotoSansCJKsc-Regular.otf"
 const NotoSansCJKscMedium = "https://mirrors.tuna.tsinghua.edu.cn/ctan/fonts/notocjksc/NotoSansCJKsc-Medium.otf"
 
